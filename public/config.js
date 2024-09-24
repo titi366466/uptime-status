bb17cf54280cf5caca91fbcde57f8e04a7ef8aa2
titi366466 <158862938+titi366466@users.noreply.github.com>
2024-09-24 14:54:16 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,7 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'u2580262-a75d09cc5ca27ac208d3a459', // 全局 API 密钥
+    'u2580262-b6d1aa88694d6c95442ceec8', // 全局 API 密钥
   ],
 
   // 日志天数
